690e749e6b25aae7084a0f9ec3ff4f5e8e973bf8
layton <jiajun.huang@thoughtworks.com>
2024-03-22 15:04:41 +0800

    test: add test for productDisplayInfo.getRealPrice

--- src/test/kotlin/com/thoughtworks/assignment/ProductDisplayInfoTest.kt
+++ src/test/kotlin/com/thoughtworks/assignment/ProductDisplayInfoTest.kt
@@ -7,8 +7,38 @@ import kotlin.test.assertEquals
 
 class ProductDisplayInfoTest {
     @Test
-    fun `should return right price when product type is normal`() {
+    fun `should return right price when product type is NORMAL`() {
         val productDisplayInfo = ProductDisplayInfo("sku", "name", 5.0, 10, "image.jpg", ProductType.NORMAL)
         assertEquals(5.0, productDisplayInfo.getRealPrice())
+    }
+
+    @Test
+    fun `should return right price when product type is HIGH_DEMAND and quantity bigger than 100`() {
+        val productDisplayInfo = ProductDisplayInfo("sku", "name", 10.0, 101, "image.jpg", ProductType.HIGH_DEMAND)
+        assertEquals(10.0, productDisplayInfo.getRealPrice())
+    }
+
+    @Test
+    fun `should return right price when product type is HIGH_DEMAND and quantity is 100`() {
+        val productDisplayInfo = ProductDisplayInfo("sku", "name", 10.0, 100, "image.jpg", ProductType.HIGH_DEMAND)
+        assertEquals(12.0, productDisplayInfo.getRealPrice())
+    }
+
+    @Test
+    fun `should return right price when product type is HIGH_DEMAND and quantity is less than 100 and more than 30`() {
+        val productDisplayInfo = ProductDisplayInfo("sku", "name", 10.0, 50, "image.jpg", ProductType.HIGH_DEMAND)
+        assertEquals(12.0, productDisplayInfo.getRealPrice())
+    }
+
+    @Test
+    fun `should return right price when product type is HIGH_DEMAND and quantity is 30`() {
+        val productDisplayInfo = ProductDisplayInfo("sku", "name", 10.0, 30, "image.jpg", ProductType.HIGH_DEMAND)
+        assertEquals(15.0, productDisplayInfo.getRealPrice())
+    }
+
+    @Test
+    fun `should return right price when product type is HIGH_DEMAND and quantity is less than 30`() {
+        val productDisplayInfo = ProductDisplayInfo("sku", "name", 10.0, 29, "image.jpg", ProductType.HIGH_DEMAND)
+        assertEquals(15.0, productDisplayInfo.getRealPrice())
     }
 }

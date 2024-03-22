f87b1f2bca4f67dd8f7006b01eaa6b96ffeddde1
layton <jiajun.huang@thoughtworks.com>
2024-03-22 17:36:48 +0800

    refactor: extract class variable for common test

--- src/test/kotlin/com/thoughtworks/assignment/ProductDisplayInfoTest.kt
+++ src/test/kotlin/com/thoughtworks/assignment/ProductDisplayInfoTest.kt
@@ -6,40 +6,42 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class ProductDisplayInfoTest {
+    private val initialPrice = 10.0
+
     @Test
     fun `should return right price when product type is NORMAL`() {
-        val productDisplayInfo = buildProductDisplayInfoWithDefaultValue(ProductType.NORMAL, 120, 5.0)
-        assertEquals(5.0, productDisplayInfo.getRealPrice())
+        val productDisplayInfo = buildProductDisplayInfoWithDefaultValue(ProductType.NORMAL, 120, initialPrice)
+        assertEquals(initialPrice, productDisplayInfo.getRealPrice())
     }
 
     @Test
     fun `should return right price when product type is HIGH_DEMAND and quantity bigger than 100`() {
-        val productDisplayInfo = buildProductDisplayInfoWithDefaultValue(ProductType.HIGH_DEMAND, 101, 10.0)
-        assertEquals(10.0, productDisplayInfo.getRealPrice())
+        val productDisplayInfo = buildProductDisplayInfoWithDefaultValue(ProductType.HIGH_DEMAND, 101, initialPrice)
+        assertEquals(initialPrice, productDisplayInfo.getRealPrice())
     }
 
     @Test
     fun `should return right price when product type is HIGH_DEMAND and quantity is 100`() {
-        val productDisplayInfo = buildProductDisplayInfoWithDefaultValue(ProductType.HIGH_DEMAND, 100, 10.0)
-        assertEquals(12.0, productDisplayInfo.getRealPrice())
+        val productDisplayInfo = buildProductDisplayInfoWithDefaultValue(ProductType.HIGH_DEMAND, 100, initialPrice)
+        assertEquals(initialPrice * 1.2, productDisplayInfo.getRealPrice())
     }
 
     @Test
     fun `should return right price when product type is HIGH_DEMAND and quantity is less than 100 and more than 30`() {
-        val productDisplayInfo = buildProductDisplayInfoWithDefaultValue(ProductType.HIGH_DEMAND, 50, 10.0)
-        assertEquals(12.0, productDisplayInfo.getRealPrice())
+        val productDisplayInfo = buildProductDisplayInfoWithDefaultValue(ProductType.HIGH_DEMAND, 50, initialPrice)
+        assertEquals(initialPrice * 1.2, productDisplayInfo.getRealPrice())
     }
 
     @Test
     fun `should return right price when product type is HIGH_DEMAND and quantity is 30`() {
-        val productDisplayInfo = buildProductDisplayInfoWithDefaultValue(ProductType.HIGH_DEMAND, 30, 10.0)
-        assertEquals(15.0, productDisplayInfo.getRealPrice())
+        val productDisplayInfo = buildProductDisplayInfoWithDefaultValue(ProductType.HIGH_DEMAND, 30, initialPrice)
+        assertEquals(initialPrice * 1.5, productDisplayInfo.getRealPrice())
     }
 
     @Test
     fun `should return right price when product type is HIGH_DEMAND and quantity is less than 30`() {
-        val productDisplayInfo = buildProductDisplayInfoWithDefaultValue(ProductType.HIGH_DEMAND, 29, 10.0)
-        assertEquals(15.0, productDisplayInfo.getRealPrice())
+        val productDisplayInfo = buildProductDisplayInfoWithDefaultValue(ProductType.HIGH_DEMAND, 29, initialPrice)
+        assertEquals(initialPrice * 1.5, productDisplayInfo.getRealPrice())
     }
 
     private fun buildProductDisplayInfoWithDefaultValue(

fe29f64bc02167c9ac5cd8910c728f8c845aaee7
layton <jiajun.huang@thoughtworks.com>
2024-03-22 17:08:24 +0800

    refactor: extract baseUrl to config.properties

--- src/main/kotlin/com/thoughtworks/assignment/RetrofitManager.kt
+++ src/main/kotlin/com/thoughtworks/assignment/RetrofitManager.kt
@@ -2,12 +2,23 @@ package com.thoughtworks.assignment
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 
 object RetrofitManager {
-    private val retrofit = Retrofit.Builder()
-        .baseUrl("http://localhost:3000")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+
+    private val retrofit: Retrofit
+
+    init {
+        val properties = Properties()
+        val inputStream = ClassLoader.getSystemClassLoader().getResourceAsStream("config.properties")
+        properties.load(inputStream)
+        val baseUrl = properties.getProperty("base_url")
+
+        retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 
     val productApiService: ProductApiService = retrofit.create(ProductApiService::class.java)
 }

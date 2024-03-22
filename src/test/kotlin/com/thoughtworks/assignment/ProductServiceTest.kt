8a9ae0dd48a6b4cbd1eb423776e39bd424da92a6
layton <jiajun.huang@thoughtworks.com>
2024-03-22 17:23:11 +0800

    refactor: extract class variable and common mock logic

--- src/test/kotlin/com/thoughtworks/assignment/ProductServiceTest.kt
+++ src/test/kotlin/com/thoughtworks/assignment/ProductServiceTest.kt
@@ -13,16 +13,24 @@ import kotlin.test.assertEquals
 
 class ProductServiceTest {
 
+    private val mockApiService = mockk<ProductApiService>()
+    private val productService = ProductService(mockApiService)
+
+    private fun mockProductRequestResult(
+        mockProductInfoList: ArrayList<ProductInfo>,
+        mockProductInventoryList: ArrayList<ProductInventory>
+    ) {
+        coEvery { mockApiService.getProductInfoList() }.returns(mockProductInfoList)
+        coEvery { mockApiService.getProductInventoryList() }.returns(mockProductInventoryList)
+    }
+
     @Test
     fun `should get product display info when hava one to one info`() {
         // given
-        val mockApiService = mockk<ProductApiService>()
         val mockProductInfoList = arrayListOf(ProductInfo("1", "ABC", "product1", 5.0, "NORMAL", "image3.jpg"))
         val mockProductInventoryList = arrayListOf(ProductInventory("1", "ABC", "china", 5))
-        coEvery { mockApiService.getProductInfoList() }.returns(mockProductInfoList)
-        coEvery { mockApiService.getProductInventoryList() }.returns(mockProductInventoryList)
+        mockProductRequestResult(mockProductInfoList, mockProductInventoryList)
         // when
-        val productService = ProductService(mockApiService)
         val productDisplayInfoList = productService.generateProductDisplayList()
         // then
         assertEquals(
@@ -34,16 +42,13 @@ class ProductServiceTest {
     @Test
     fun `should get correct quantity when one product have two zone`() {
         // given
-        val mockApiService = mockk<ProductApiService>()
         val mockProductInfoList = arrayListOf(ProductInfo("1", "ABC", "product1", 5.0, "NORMAL", "image3.jpg"))
         val mockProductInventoryList = arrayListOf(
             ProductInventory("1", "ABC", "china", 5),
             ProductInventory("2", "ABC", "England", 3)
         )
-        coEvery { mockApiService.getProductInfoList() }.returns(mockProductInfoList)
-        coEvery { mockApiService.getProductInventoryList() }.returns(mockProductInventoryList)
+        mockProductRequestResult(mockProductInfoList, mockProductInventoryList)
         // when
-        val productService = ProductService(mockApiService)
         val productDisplayInfoList = productService.generateProductDisplayList()
         // then
         assertEquals(
@@ -55,7 +60,6 @@ class ProductServiceTest {
     @Test
     fun `should get correct quantity when multiple products have multiple zone`() {
         // given
-        val mockApiService = mockk<ProductApiService>()
         val mockProductInfoList = arrayListOf(
             ProductInfo("1", "ABC", "product1", 5.0, "NORMAL", "image3.jpg"),
             ProductInfo("2", "SKU2", "product2", 5.0, "NORMAL", "image4.jpg")
@@ -66,10 +70,8 @@ class ProductServiceTest {
             ProductInventory("3", "SKU2", "America", 5),
             ProductInventory("4", "SKU2", "India", 3)
         )
-        coEvery { mockApiService.getProductInfoList() }.returns(mockProductInfoList)
-        coEvery { mockApiService.getProductInventoryList() }.returns(mockProductInventoryList)
+        mockProductRequestResult(mockProductInfoList, mockProductInventoryList)
         // when
-        val productService = ProductService(mockApiService)
         val productDisplayInfoList = productService.generateProductDisplayList()
         // then
         assertEquals(
@@ -83,13 +85,10 @@ class ProductServiceTest {
     @Test
     fun `should throw Exception when one product sku can not find in product inventory`() {
         // given
-        val mockApiService = mockk<ProductApiService>()
         val mockProductInfoList = arrayListOf(ProductInfo("1", "ABC", "product1", 5.0, "NORMAL", "image3.jpg"))
         val mockProductInventoryList = arrayListOf(ProductInventory("1", "AB", "china", 5))
-        coEvery { mockApiService.getProductInfoList() }.returns(mockProductInfoList)
-        coEvery { mockApiService.getProductInventoryList() }.returns(mockProductInventoryList)
+        mockProductRequestResult(mockProductInfoList, mockProductInventoryList)
         // when
-        val productService = ProductService(mockApiService)
         // then
         assertThrows<RuntimeException> {
             productService.generateProductDisplayList()
